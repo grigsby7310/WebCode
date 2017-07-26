dddf9a3e20b7b6776f7f83fcf189f0eba87af88c
grigsby7310 <grigsby7310@gmail.com>
2017-07-26 08:57:14 -0700

    Update AssemblyInfo.cs

--- CIS247_WK7_Lab_GRIGSBY/Properties/AssemblyInfo.cs
+++ CIS247_WK7_Lab_GRIGSBY/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// associated with an assembly
 [assembly: AssemblyTitle("CIS247_WK7_Lab_GRIGSBY")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
